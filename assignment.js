0d1d6ea9d0baf25c380b30b30b5922f47a9f6d02
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-09 15:25:02 +0100

    update

--- assignment.js
+++ assignment.js
@@ -1,13 +1,16 @@
 // LECTURE: INtroduction to Arrays 
 const poopulation = [4100 , 1200 , 1230 , 1400] ; 
+//checking if its 4 values  true / false ?
  console.log (poopulation.length===4)
+
 function precentage0fWorld1 (population){
     return (population /7900) *100
 }
+//putting array in function so its easy to log in console
 const percentages = [
 precentage0fWorld1 (poopulation[1]),
 precentage0fWorld1 (poopulation[2]),
 precentage0fWorld1 (poopulation[3]),
 precentage0fWorld1 (poopulation[0])]
 
-console.log(percentages)
+console.log(percentages);
