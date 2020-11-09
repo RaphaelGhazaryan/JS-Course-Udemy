9b4da2753d3e363707534e989c1a38732f46702b
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-09 15:22:54 +0100

    course

---
+++ assignment.js
@@ -0,0 +1,13 @@
+// LECTURE: INtroduction to Arrays 
+const poopulation = [4100 , 1200 , 1230 , 1400] ; 
+ console.log (poopulation.length===4)
+function precentage0fWorld1 (population){
+    return (population /7900) *100
+}
+const percentages = [
+precentage0fWorld1 (poopulation[1]),
+precentage0fWorld1 (poopulation[2]),
+precentage0fWorld1 (poopulation[3]),
+precentage0fWorld1 (poopulation[0])]
+
+console.log(percentages)
