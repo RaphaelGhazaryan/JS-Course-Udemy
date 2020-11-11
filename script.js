cc2ceaeb435ca76ef57feb213fa5d76dc7715974
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-11 15:44:21 +0100

    asd

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const calcTip = (billValue) => {
+    billValue >=50 && <=300 ? billValue*0.15: billValue*0.20;
+    //return retirment;
+    return `The tip is gonna be ${billValue}€`
+}
+age >=18 ? console.log(firstName + " drinks beer.")
+: console.log(firstName + " drinks juice.");
+
