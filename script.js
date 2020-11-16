0e364e8398b70a823e960cc1f366acfaa285ad3d
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-16 10:46:19 +0100

    finishedTheCodingChallenge

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ console.log(result);
 //shorter version
 const calcTipp = billValue => billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.20;
 console.log(calcTipp(100));
+
+//calculating tips from arrays 
+const bills = [125,555,44];
+const tips = [calcTip(125),calcTip(555),calcTip(44)];
+console.log(tips); 
+
+//tip with bill value in arrays
+const total = [125 + calcTip(125),555 + calcTip(555),44 + calcTip(44)]
+console.log(total);
