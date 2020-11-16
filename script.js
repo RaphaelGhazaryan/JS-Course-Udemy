af2ce1f81f5c93b6a927da22777f6855f27558af
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-16 11:15:36 +0100

    completedAssignmentAndLectureFromJscourse

---
+++ script.js
@@ -0,0 +1,22 @@
+//Objects
+
+
+//array
+const raphaelArray = [
+    'Raphael',
+    'Ghazaryan',
+    2020-1999,
+    'Student',
+    ['Sahel','Erik', 'Wasen'],
+];
+
+//object
+const raphael = {
+    firstName: 'Raphael', 
+    lastName: 'Ghazaryan',
+    age: 2020-1999,
+    job: 'Student',
+    friends: ['Sahel','Erik', 'Wasen']
+};
+
+console.log (raphael)
