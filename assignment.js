8816b1de5c087e7d59bcc606580361dae77deb98
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-17 10:57:39 +0100

    changeSolution

--- assignment.js
+++ assignment.js
@@ -5,8 +5,7 @@ const myCountry = {
     population: '82000000',
     neighbours: ['Poland','Netherland','France','Tsech']
 };
-myCountry.population = '84000000';
-myCountry['population'] = '82000000';
+
 console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language} people,
 ${myCountry.neighbours.length} neigbouring countries and a capital called ${myCountry.capital}.`);
 
