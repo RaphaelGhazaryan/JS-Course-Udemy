4d4af622244eebd53b8bab17b1ecd28936fbf0e3
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-23 14:58:29 +0100

    forLoopBreakContinue

--- assignment.js
+++ assignment.js
@@ -1,5 +1,16 @@
     // Loop assignment
 
-    for(let rep = 1; rep <= 50; rep ++ );{
+    for(let rep = 1; rep <= 1; rep ++ )
         console.log(`Voter number ${rep} is currently voting `);
-    }
+ 
+
+        function percentageOfWorld1(population) {
+            return (population / 7900) * 100;
+            }
+ const populations = [10, 1441, 332, 83];
+ const percentages2 = [];
+ for (let i = 0; i < populations.length; i++) {
+ const perc = percentageOfWorld1(populations[i]);
+ percentages2.push(perc);
+ }
+ console.log(percentages2);
