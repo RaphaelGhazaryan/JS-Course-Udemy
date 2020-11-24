1dc41143ff181dc62a4acc731d3631610308bcc7
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-24 09:01:39 +0100

    mistakefixed

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ for(let i = raphael.length - 1; i >= 0; i--) {
 // loops in loops
 
 for (let exercise = 1; exercise <= 3; exercise++) {
-    console.log(`---EXERCISE NR.${exercise}`)
+    console.log(`---EXERCISE NR.${exercise}`);
 
  for (let rep = 1; rep < 6; rep++) {
     console.log(`Lifting weight repetition ${rep}`);
