d6bfcae966bf13ae502fa800596c6d36635006a5
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-25 11:21:13 +0100

    test

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-// looping backwards
+// Task
+const flatNeighbours = []
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+function makeItFlat(array) { // [[country1, country2], [country3]]
+  // returns [country1, country2, country3]
+  
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+const flatNeighbours = []
+const types = typeof 'listOfNeighbours[1]';
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    const currentItem = listOfNeighbours[i];
+    if (flatNeighbours.includes(types) === false) {
+      flatNeighbours.push(currentItem);
+    }
+  }
 
-const raphael = [
-    'Raphael',
-    'Ghazaryan',
-    2020 - 1999,
-    'student',
-    ['Wasen','Sahel','Erik'],
-];
-
-
-for(let i = raphael.length - 1; i >= 0; i--) {
-    console.log(i,raphael[i]);
+  return flatNeighbours;
 }
 
-// loops in loops
+console.log(makeItFlat(flatNeighbours))
 
-for (let exercise = 1; exercise <= 3; exercise++) {
-    console.log(`---EXERCISE NR.${exercise}`);
+// Task 2 
 
- for (let rep = 1; rep < 6; rep++) {
-    console.log(`Lifting weight repetition ${rep}`);
-     
- }
-}
+
+function drawTree(count) {
+    for (let star = 0; star <= count; star++){
+        let starr = '';
+   
+        for (let stars = 0 ; stars <=1; stars++){
+        starr += "  ";
+        };
+        for (let starssi = 0; starssi <= star; starssi++) {
+            starr += "*";    
+        };
+        console.log(starr);
+    };
+
+};
+
+drawTree(30);
+
+
+  
