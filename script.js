e43d8f3cbdfeb6d4bef43b28cddffce88ea1657f
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2020-11-30 08:39:02 +0100

    correction

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Task
-const flatNeighbours = []
+const flatNeighbours = [];
 const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
 function makeItFlat(array) { // [[country1, country2], [country3]]
   // returns [country1, country2, country3]
   
 const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
-const flatNeighbours = []
+const flatNeighbours = [];
 const types = typeof 'listOfNeighbours[1]';
 for (let i = 0; i < listOfNeighbours.length; i++) {
     const currentItem = listOfNeighbours[i];
@@ -17,7 +17,7 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
   return flatNeighbours;
 }
 
-console.log(makeItFlat(flatNeighbours))
+console.log(makeItFlat(flatNeighbours));
 
 // Task 2 
 
