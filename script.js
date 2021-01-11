24cc784e3291112e6363a89c4d0d751113f5745c
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2021-01-11 16:23:22 +0100

    checkout

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //Dom Manipulation and Selection
 
-// for textcontent
+/*// for textcontent
 console.log(document.querySelector('.message').textContent);
 console.log(document.querySelector('.number').textContent);
 console.log(document.querySelector('.score').textContent);
@@ -12,9 +12,12 @@ document.querySelector('.score').textContent = 13;
 document.querySelector('.number').textContent = 20;
 
 // for input field
-document.querySelector('.guess').value = 10;
+document.querySelector('.guess').value = 10; */
 
 //Click Events
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,5 +26,15 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent =
       'Please type in a valid number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number !! Woho';
+  } else if (guess > secretNumber) {
+    (document.querySelector('.message').textContent = 'Too high! Try again'),
+      score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    (document.querySelector('.message').textContent = 'Too low! Try again'),
+      score--;
+    document.querySelector('.score').textContent = score;
   }
 });
