ca34e9f822be28454c79238bb26fdf528bee4e5d
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2021-01-11 14:03:43 +0100

    asd

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 'use strict';
 
-document.querySelector('.btn').addEventListener('click', function () {
-  var guess = document.querySelector('input');
-  guess.onkeyup = function () {
-    document.getElementById('content').innerHTML = guess.value;
-  };
-
-  if (!guess) {
-    document.querySelector('.message').textContent =
-      'Please type in something valid';
-  }
-});
+function pr() {
+  document.getElementById('result').innerHTML = document.getElementById(
+    'lname'
+  ).value;
+}
