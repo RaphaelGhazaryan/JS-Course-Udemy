ad38036e2988442a3c926e8198f3a86df27ff0b6
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2021-01-19 16:43:00 +0100

    asd

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close.modal');
+const btns = document.querySelectorAll('.show-modal');
+
+console.log(btns);
+
+for (let i = 0; i < btns.length; i++) console.log(btns[i].textContent);
