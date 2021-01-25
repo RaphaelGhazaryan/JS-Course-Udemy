06a61256e173a4f85c958fb81be67bcac7e19731
Raphael Ghazaryan <raphael.ghaz@gmail.com>
2021-01-25 12:05:51 +0100

    asd

--- script.js
+++ script.js
@@ -2,9 +2,25 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close.modal');
+const btn = document.querySelector('.close-modal');
 const btns = document.querySelectorAll('.show-modal');
 
-console.log(btns);
+// saving space and not repeating code
+const openModal = function(){
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btns.length; i++) console.log(btns[i].textContent);
+// opening modal on btn click and blurr overlay
+for (let i = 0; i < btns.length; i++)
+btns[i].addEventListener('click', openModal);
+
+// creating function to close modal to save space and not to repeat code
+const closeModal = function(){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+// closing modal on btn click and on overlay click
+btn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal)
