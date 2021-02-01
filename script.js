2fec12e8e6b46b9f8059a1546d95cb0ca9dc8298
Raphael Ghazaryan <raphael.ghaz@gmail.com>
2021-02-01 15:22:20 +0100

    asd

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 'use strict';
 
-const rollDice = document.querySelector('btn btn--roll'); 
-const rolling = Math.trunc(Math.random() * 6);
-rollDice.addEventListener('click', rolling)
+// Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1')
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn.-new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
+
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function(){
+    // Generating random dice roll 
+    const dice = Math.trunc(Math.random()*6) + 1;
+    // Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // Check if 1  if true, switch to next player
+    if(dice !== 1) {
+        // Add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore;
+    } else {
+    }
+});
