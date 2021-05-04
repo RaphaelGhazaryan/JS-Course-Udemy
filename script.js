d3186b8dd5082772f8c121a53ac24259793a7767
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2021-05-04 14:00:25 +0200

    asd

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let age = 21;
+let oldAge = age;
+age = 22;
+console.log(age, oldAge);
+
+const me = {
+  name: 'Raphael',
+  age: 21,
+};
+const friend = me;
+friend.age = 24;
+console.log(friend, me);
