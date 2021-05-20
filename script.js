ecbedb773d07ff8c1d2c12ce78ec3c88ad27abf9
RaphaelGhazaryan <raphael.ghaz@gmail.cm>
2021-05-20 16:16:13 +0200

    spread

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    Mo: { opentimes: 11 - 22 },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[starterIndex]];
+  },
 };
 
 const arr = [2, 3, 4];
@@ -17,3 +23,41 @@ const c = arr[2];
 const [x, y, z] = arr;
 
 console.log(x, y, z);
+console.log(arr);
+
+let [first, second] = restaurant.categories;
+console.log(first, second);
+
+const temp = first;
+first = second;
+second = temp;
+console.log(first, second);
+[second, first] = [first, second];
+console.log(first, second);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [6, 8]];
+/* const [i, , j] = nested;
+console.log(i, j) */ const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Destrucuring objects
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// The Spread Operator ...
+const array = ['alpha', 'beta', 'gamma', 'Raphaeil ist operator'];
+const evilArray = [array[0], array[1], array[2], 'Delta', 'Epsilon'];
+const evenMoreEvilArray = [...array, ...evilArray];
+console.log(evenMoreEvilArray);
+
+const newMenu = [...restaurant.mainMenu, 'Xorovatz'];
+console.log(newMenu);
+
+const mainMenuCopy = [...restaurant.mainMenu];
+const menu = [...restaurant.starterMenu];
+const completeMenu = [...menu, ...mainMenuCopy];
+console.log(completeMenu);
